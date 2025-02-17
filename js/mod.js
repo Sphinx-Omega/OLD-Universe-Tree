e8b57fbf491854aeec6d0268addb89db8dd1b481
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-17 21:50:02 +0000

    Created the Universe Tree

--- js/mod.js
+++ js/mod.js
@@ -1,25 +1,25 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Universe Tree",
+	author: "Sphinx-Omega",
+	pointsName: "particles",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Singularity",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Universe begins.<br>
+		- Added particles.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
