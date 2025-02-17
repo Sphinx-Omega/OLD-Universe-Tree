e4bec0e91f44f64c506f46bd950962ca92bc08a8
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-17 23:49:09 +0000

    added upgrade 5

--- js/layers.js
+++ js/layers.js
@@ -61,5 +61,16 @@ addLayer("p", {
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
+
+        22: {
+            title: "Overdrive",
+            description: "Particles split and fuse way faster. Quarks and particles boost each other more.",
+            cost: new Decimal(50000),
+
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5), player.points.add(1).pow(0.25)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
     },
 })
