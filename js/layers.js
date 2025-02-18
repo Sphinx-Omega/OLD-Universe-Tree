042ca8cb2c81aa4704bbf078c5c9ccb7db52665e
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-18 01:17:24 +0000

    a

--- js/layers.js
+++ js/layers.js
@@ -16,7 +16,7 @@ addLayer("p", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
-        if (hasUpgrade('p', 22)) mult = mult.times(upgradeEffect('p', 22)/1.5)
+        if (hasUpgrade('p', 22)) mult = mult.times(upgradeEffect('p', 22).div(1.5))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
