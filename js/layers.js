af9303f22492beeaec1e998ce4093f23ebb99ddd
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-18 00:55:51 +0000

    fixed layer placements

--- js/layers.js
+++ js/layers.js
@@ -80,7 +80,7 @@ addLayer("p", {
 addLayer("a", {
     name: "achievements", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: side, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -123,7 +123,7 @@ addLayer("e", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "e", description: "E: Electron reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
