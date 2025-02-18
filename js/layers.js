a660b958542aa09d2757d9c308e88faf7fb3200d
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-18 01:57:40 +0000

    fixed electrons being based on particles instead of quarks

--- js/layers.js
+++ js/layers.js
@@ -114,7 +114,7 @@ addLayer("e", {
     requires: new Decimal(1000000), // Can be a function that takes requirement increases into account
     resource: "electrons", // Name of prestige currency
     baseResource: "quarks", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
