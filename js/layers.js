cf32eb583ed498a02a515b157941e8a67eac9e32
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-19 18:45:55 +0000

    electrons fixed, atom effect works

--- js/layers.js
+++ js/layers.js
@@ -153,6 +153,7 @@ addLayer("e", {
     branches: ["p"],
     gainMult() { // Calculate the multiplier for main currency from bonuses
         emult = new Decimal(1)
+        emult = emult.mul(tmp.t.effect).mul(tmp.t.effect)
         if (hasUpgrade('e', 13)) emult = emult.times(upgradeEffect('e', 13))
         if (hasUpgrade('e', 23)) emult = emult.times(upgradeEffect('e', 23))
         return emult
