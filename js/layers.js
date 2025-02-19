83d6e677d293511f9ca3d233e2d9e365e6dc7d79
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-19 23:11:55 +0000

    i

--- js/layers.js
+++ js/layers.js
@@ -369,7 +369,7 @@ addLayer("t", {
             unlocked() {return true},
 
             effect() {
-                return player[this.layer].points.add(1).mul(10).log10()
+                return player[this.layer].points.add(1).log10().div(42)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
         },
