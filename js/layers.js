ee6412cc3bc9c4fbe4093b78ffca861907106224
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-19 20:38:25 +0000

    slight e upg 23 nerf

--- js/layers.js
+++ js/layers.js
@@ -271,7 +271,7 @@ addLayer("e", {
             unlocked() {return true},
 
             effect() {
-                return player[this.layer].points.add(1).pow(0.33)
+                return player[this.layer].points.add(1).pow(0.3)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
         },
