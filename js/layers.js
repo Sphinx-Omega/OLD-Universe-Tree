0bb03a3eddc1c59f6f0dac78800882c079fde41a
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-19 22:21:29 +0000

    atom upg 11 buff

--- js/layers.js
+++ js/layers.js
@@ -369,7 +369,7 @@ addLayer("t", {
             unlocked() {return true},
 
             effect() {
-                return player[this.layer].points.add(1).pow(0.25)
+                return player[this.layer].points.add(1).pow(0.5).log10()
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
         },
