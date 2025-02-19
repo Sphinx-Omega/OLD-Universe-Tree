9c0ed2cbcafada3c15ce37e91a71bc2322d31ef6
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-02-19 20:36:03 +0000

    buffed e upg 23 (for real this time)

--- js/layers.js
+++ js/layers.js
@@ -271,7 +271,7 @@ addLayer("e", {
             unlocked() {return true},
 
             effect() {
-                return player[this.layer].points.add(2).pow(0.15)
+                return player[this.layer].points.add(1).pow(0.5)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
         },
